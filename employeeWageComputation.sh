b504459f99d23cf9d551ba3afb2c014922e7d370
saileshchauhan <chauhansailesh68@gmail.com>
2021-03-30 15:29:52 +0530

    Adittion UC4

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -4,18 +4,23 @@ fullDayHours=8
 wagePerHour=20
 partTimeHour=8
 
-isPresent=1
-isAbsent=0
-isPartTime=2
-randomNumber=$((RANDOM%3))
 
-if [ $randomNumber -eq 1 ]
-then
-	echo "Present"
-	dailyEmployeeWage=$(($wagePerHour*$fullDayHours))
-elif [ $randomNumber -eq 2 ]
-then
-	echo "Part Time"
-	partTimeEmployeeWage=$(($wagePerHour*$partTimeHour))
-fi
+feq=$((RANDOM%3))
+i=$feq
 
+case $i in
+	1)
+		echo "Present:"$i
+		employewage=$(($fullDayHours*$wagePerHour))
+		;;
+	2)
+		echo "partTime:"$i
+		employewage=$(($partTimeHour*$wagePerHour))
+		;;
+	0)
+		echo "Absent:"
+		;;
+	*)
+		echo "Invalid Input"
+		;;
+esac
