c090ed315bcaea2eb5162f6a2bd64b8b786dd30d
saileshchauhan <chauhansailesh68@gmail.com>
2021-03-30 16:22:53 +0530

    Addition UC6

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -3,24 +3,34 @@
 fullDayHours=8
 wagePerHour=20
 partTimeHour=8
-workDayinMonth=20
+workDayinMonth=0
+employeWageForAMonth=0
 
-feq=$((RANDOM%3))
-i=$feq
+#Total wages till total working hours 100 or 20 day in a month
 
-case $i in
-	1)
-		echo "Present:"$i
-		employeWageForAMonth=$(($fullDayHours*$wagePerHour*))
-		;;
-	2)
-		echo "partTime:"$i
-		employeWageForAMonth=$(($partTimeHour*$wagePerHour))
-		;;
-	0)
-		echo "Absent:"
-		;;
-	*)
+while [ $workDayinMonth -ne 20 -a $fullDayHours -ne 100 ]
+do
+
+	feq=$((RANDOM%3))
+	i=$feq
+
+	case $i in
+		1)
+			echo "Present:"$i
+			employeWageForAMonth=$(($employeWageForAMonth+$(($fullDayHours*$wagePerHour))))
+			;;
+		2)
+			echo "partTime:"$i
+			employeWageForAMonth=$(($employeWageForAMonth+$(($partTimeHour*$wagePerHour))))
+			;;
+		0)
+			echo "Absent:"
+			;;
+		*)
 		echo "Invalid Input"
-		;;
-esac
+			;;
+	esac
+	 workDayinMonth=$(($workDayinMonth+1))
+	fullDayHours=$(($fullDayHours+8))
+	partTimeHour=$(($fullDayHours+8))
+done
