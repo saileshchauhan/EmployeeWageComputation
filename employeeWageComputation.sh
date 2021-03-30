57254714aa514e7f181e63df14fca0ba6179e130
saileshchauhan <chauhansailesh68@gmail.com>
2021-03-30 18:41:55 +0530

    Refactor

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -2,35 +2,46 @@
 
 fullDayHours=8
 wagePerHour=20
-partTimeHour=8
+partTimeHour=4
 workDayinMonth=0
 employeWageForAMonth=0
 
 #Total wages till total working hours 100 or 20 day in a month
 
+function getWorkHrs(){
+	local i=$empCheck
+		 case $i in
+      	1)
+         	empHrs=8
+         ;;
+      	2)
+         	empHrs=4
+         ;;
+      	0)
+         	empHrs=0
+         ;;
+      	*)
+      	echo "Invalid Input"
+         ;;
+   esac
+	echo $empHrs
+}
+
+
 while [ $workDayinMonth -ne 20 -a $fullDayHours -ne 100 ]
 do
 
-	feq=$((RANDOM%3))
-	i=$feq
+	empCheck=$((RANDOM%3))
+	empHrs="$(getWorkHrs $empCheck)"
+	employeWageForAMonth=$(($employeWageForAMonth+$(($empHrs*$wagePerHour))))
 
-	case $i in
-		1)
-			echo "Present:"$i
-			employeWageForAMonth=$(($employeWageForAMonth+$(($fullDayHours*$wagePerHour))))
-			;;
-		2)
-			echo "partTime:"$i
-			employeWageForAMonth=$(($employeWageForAMonth+$(($partTimeHour*$wagePerHour))))
-			;;
-		0)
-			echo "Absent:"
-			;;
-		*)
-		echo "Invalid Input"
-			;;
-	esac
-	 workDayinMonth=$(($workDayinMonth+1))
-	fullDayHours=$(($fullDayHours+8))
-	partTimeHour=$(($fullDayHours+8))
+
+
+	workDayinMonth=$(($workDayinMonth+1))
+
+	fullDayHours=$(($empHrs+8))
+	partTimeHour=$(($empHrs+8))
+
 done
+
+
