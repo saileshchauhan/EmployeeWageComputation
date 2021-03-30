9521f112a54e19699541ca3fdbac03f489e4d9f9
saileshchauhan <chauhansailesh68@gmail.com>
2021-03-30 14:53:29 +0530

    Addition UC3

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -2,6 +2,20 @@
 
 fullDayHours=8
 wagePerHour=20
+partTimeHour=8
 
+isPresent=1
+isAbsent=0
+isPartTime=2
+randomNumber=$((RANDOM%3))
 
-dailyEmployeeWage=$(($wagePerHour*$fullDayHours))
+if [ $randomNumber -eq 1 ]
+then
+	echo "Present"
+	dailyEmployeeWage=$(($wagePerHour*$fullDayHours))
+elif [ $randomNumber -eq 2 ]
+then
+	echo "Part Time"
+	partTimeEmployeeWage=$(($wagePerHour*$partTimeHour))
+fi
+
