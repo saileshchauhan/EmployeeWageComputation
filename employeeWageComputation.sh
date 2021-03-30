4ed3afab0e154a038944c265e59e89403a21869c
saileshchauhan <chauhansailesh68@gmail.com>
2021-03-30 14:25:53 +0530

    Addition UC2

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -2,9 +2,12 @@
 present=1
 absent=0
 randomNumber=$((RANDOM%2))
+workHours=8
+salaryperHour=50
 if [ $randomNumber -eq 1 ]
 then
 	echo "Present"
+	dailyEmployeeWage=$(($workHours*$salaryperHour))
 else
 	echo "Absent"
 fi
