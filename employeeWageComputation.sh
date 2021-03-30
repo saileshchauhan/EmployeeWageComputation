b26bec13f9b82721766c6304bf11ec9068b4070c
saileshchauhan <chauhansailesh68@gmail.com>
2021-03-30 14:33:14 +0530

    Addition UC2

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,13 +1,7 @@
 #!/bin/bash -x
-present=1
-absent=0
-randomNumber=$((RANDOM%2))
-workHours=8
-salaryperHour=50
-if [ $randomNumber -eq 1 ]
-then
-	echo "Present"
-	dailyEmployeeWage=$(($workHours*$salaryperHour))
-else
-	echo "Absent"
-fi
+
+fullDayHours=8
+wagePerHour=20
+
+
+dailyEmployeeWage=$(($wagePerHour*$fullDayHours))
