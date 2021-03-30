955551591c4f5fc101151154d9865d91565fd5f7
saileshchauhan <chauhansailesh68@gmail.com>
2021-03-30 15:35:54 +0530

    Addition UC5

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -3,7 +3,7 @@
 fullDayHours=8
 wagePerHour=20
 partTimeHour=8
-
+workDayinMonth=20
 
 feq=$((RANDOM%3))
 i=$feq
@@ -11,11 +11,11 @@ i=$feq
 case $i in
 	1)
 		echo "Present:"$i
-		employewage=$(($fullDayHours*$wagePerHour))
+		employeWageForAMonth=$(($fullDayHours*$wagePerHour*))
 		;;
 	2)
 		echo "partTime:"$i
-		employewage=$(($partTimeHour*$wagePerHour))
+		employeWageForAMonth=$(($partTimeHour*$wagePerHour))
 		;;
 	0)
 		echo "Absent:"
